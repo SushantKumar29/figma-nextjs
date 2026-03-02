786180848b94b9d5c7041e989ba5d31a28b52b57
“SushantKumar29” <sushantkmr29@gmail.com>
2026-03-02 18:47:29 +0530

    Fix CI issue

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "^next/image$": "<rootDir>/__mocks__/image.tsx",
